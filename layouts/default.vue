a74125f5d321e989ddd9f992dcae2eb4a745b351
akifkadioglu <akif.kadioglu.28@gmail.com>
2023-12-07 17:16:12 +0300

    ufak bir şekilde başladım

---
+++ layouts/default.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container mx-auto">
+    <div
+      class="mx-2 flex items-center justify-between gap-3 h-[--header-height]"
+    >
+      <div class="lg:flex-1 flex items-center gap-1.5">
+        <span class="font-semibold">Akif's Tool</span>
+        <img src="/logo.webp" alt="logo" height="75" width="75" />
+      </div>
+      <ul class="items-center gap-x-8 hidden lg:flex">
+        <li v-for="(item, index) in pages" :key="index">
+          <NuxtLink
+            :to="item.url"
+            class="font-bold hover:text-sky-600"
+            :class="route.path == item.url ? 'text-sky-600' : ''"
+          >
+            {{ item.title }}
+          </NuxtLink>
+        </li>
+      </ul>
+      <div class="flex items-center justify-end lg:flex-1 gap-1.5">
+        <button class="px-2 hover:bg-zinc-100">
+          <NuxtLink to="https://github.com/akifkadioglu/tool" target="_blank">
+            <Icon name="carbon:logo-github" size="25" />
+          </NuxtLink>
+        </button>
+        <button class="px-2 lg:hidden hover:bg-zinc-100">
+          <Icon name="material-symbols:list-rounded" size="25" />
+        </button>
+      </div>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute();
+const pages = [
+  {
+    url: routes.HOME,
+    title: "Home",
+  },
+  {
+    url: routes.MANIFEST,
+    title: "Manifest",
+  },
+];
+useSeoMeta({
+  title: "Awesome Tool",
+  description: "the tool generate something you want",
+  ogTitle: "Awesome Tool",
+  ogDescription: "the tool generate something you want",
+  ogImage: "/card.png",
+  ogUrl: "/card.png",
+  twitterTitle: "Awesome Tool",
+  twitterDescription: "the tool generate something you want",
+  twitterImage: "/card.png",
+  twitterCard: "summary",
+});
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+    {
+      rel: "manifest",
+      href: "/manifest.json",
+    },
+  ],
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@formkit/auto-animate",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+    "@nuxtjs/color-mode",
+  ],
+  colorMode: {
+    classSuffix: "",
+    fallback: "dark",
+  },
+  css: ["assets/app.css", "assets/animation.css"],
+});
