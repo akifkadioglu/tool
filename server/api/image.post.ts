20c3f88dac8382ca04f1e7218dd45cf941653afe
akifkadioglu <akif.kadioglu.28@gmail.com>
2023-12-09 02:33:38 +0300

    sharpı sildim

--- server/api/image.post.ts
+++ server/api/image.post.ts
@@ -1,7 +1,6 @@
-import sharp from "sharp";
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-  const img = await sharp(body).resize(320, 240).toBuffer();
-  console.log(img)
-  return img;
+  /*   const img = await sharp(body).resize(320, 240).toBuffer();
+   */
+  return body;
 });
